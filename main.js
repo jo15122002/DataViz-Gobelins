01f0dd8753deb524578291238c7883ef62f45de8
JoMobile <lapinette2002@ymail.com>
2022-11-16 10:29:48 +0100

    UPD [general] changement couleur au scroll

--- main.js
+++ main.js
@@ -2,17 +2,39 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log("Pong!");
 
-    var body = document.body
+    let lastScrollX = 0;
+    let slideNumber = 1;
     
-    console.log(body);
+    console.log(document.body);
+
+    window.scrollTo(0, 0);
     
-    window.addEventListener("scroll", () => {
+    window.addEventListener("scroll", (event) => {
         console.log(window.scrollX)
+
+        console.log(event.type)
+
+        /*if(window.scrollX > lastScrollX){
+            console.log("scrolling right")
+            slideNumber += 1
+            console.log(slideNumber)
+            event.preventDefault()
+            window.removeEventListener("scroll")
+            window.scrollTo(document.querySelector('#slide' + slideNumber), 0)
+        } else {
+            console.log("scrolling left")
+            slideNumber -= 1
+            console.log(slideNumber)
+            event.preventDefault()
+            window.removeEventListener("scroll")
+            window.scrollTo(document.querySelector('#slide' + slideNumber), 0)
+        }*/
     
         if(window.scrollX > window.innerWidth){
-            body.style.backgroundColor ="red";
+            document.body.style.backgroundColor ="red";
         }else{
-            body.style.backgroundColor ="blue";
+            document.body.style.backgroundColor ="blue";
         }
     })
+    
 })
