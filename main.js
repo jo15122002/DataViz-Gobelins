729c5476a3100adbecaa89926134fe1825a85ee0
Romain Lentz <romain@genesii.fr>
2022-11-16 15:17:57 +0100

    ADD [General] button + scroll fix

--- main.js
+++ main.js
@@ -1,35 +1,17 @@
 //Dom content loaded
 document.addEventListener('DOMContentLoaded', function() {
-    console.log("Pong!");
 
-    let lastScrollX = 0;
-    let slideNumber = 1;
-    
-    console.log(document.body);
+    document.getElementById('slide0').scrollIntoView();
 
-    window.scrollTo(0, 0);
-    
+    const NBR_SLIDE = document.querySelectorAll('.slide').length;
+
+    let btn_launch = document.querySelector('.btn-launch');
+    let btns_slides = document.querySelectorAll('.btn-slide');
+
+    document.querySelector('.slide-container').style.width = NBR_SLIDE * 100+'vw';
+
     window.addEventListener("scroll", (event) => {
-        console.log(window.scrollX)
 
-        console.log(event.type)
-
-        /*if(window.scrollX > lastScrollX){
-            console.log("scrolling right")
-            slideNumber += 1
-            console.log(slideNumber)
-            event.preventDefault()
-            window.removeEventListener("scroll")
-            window.scrollTo(document.querySelector('#slide' + slideNumber), 0)
-        } else {
-            console.log("scrolling left")
-            slideNumber -= 1
-            console.log(slideNumber)
-            event.preventDefault()
-            window.removeEventListener("scroll")
-            window.scrollTo(document.querySelector('#slide' + slideNumber), 0)
-        }*/
-    
         if(window.scrollX > window.innerWidth){
             document.body.style.backgroundColor ="red";
         }else{
@@ -37,10 +19,14 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     })
 
-    let btn_next = document.querySelector('.btn-suivant')
+    btn_launch.addEventListener('click', ()=> {
+        document.getElementById('slide1').scrollIntoView();
+    });
 
-    btn_next.addEventListener('click', ()=> {
-        window.scrollTo(window.scrollX + window.innerWidth, 0);
-    })
-
-})
+    btns_slides.forEach(btn_slide =>{
+        btn_slide.addEventListener('click', () => {
+            console.log(btn_slide.id);
+            document.getElementById('slide'+btn_slide.id).scrollIntoView();
+        });
+    });
+});
