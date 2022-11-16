75c7961a9a6ad55c9a377add394e52171d4fabee
JoMobile <lapinette2002@ymail.com>
2022-11-16 09:21:10 +0100

    ADD [general] scroll body color

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+//Dom content loaded
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Pong!");
 
+    var body = document.body
+    
+    console.log(body);
+    
+    window.addEventListener("scroll", () => {
+        console.log(window.scrollX)
+    
+        if(window.scrollX > window.innerWidth){
+            body.style.backgroundColor ="red";
+        }else{
+            body.style.backgroundColor ="blue";
+        }
+    })
+})
